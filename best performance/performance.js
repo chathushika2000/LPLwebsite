9b0f838af1b62fa9acb687cba2410fbf9459c063
Thushamini Chathushika <66440838+chathushika2000@users.noreply.github.com>
2021-06-16 10:18:37 +0530

    comit

---
+++ best performance/performance.js
@@ -0,0 +1,93 @@
+document.getElementById("card1").addEventListener("mouseover",function(){
+    document.getElementById("name").innerHTML = "Angelo Mathews"
+    document.getElementById("age").innerHTML = "38"
+    document.getElementById("type").innerHTML = "All Rounder "
+    document.getElementById("stat").innerHTML = "1020 runs";
+    document.getElementById("perform").innerHTML ="170/30"
+    document.getElementById("details").style.border="3px solid #00408c";
+    document.getElementById("details").style.background=" #cdd6e0";
+    document.getElementById("details").style.boxShadow="5px 5px 20px 10px rgba(0, 0,0, 0.5)";
+
+})
+
+document.getElementById("card2").addEventListener("mouseover",function(){
+    document.getElementById("name").innerHTML = "Christ Gayle"
+    document.getElementById("age").innerHTML = "40"
+    document.getElementById("type").innerHTML = "Batman "
+    document.getElementById("stat").innerHTML = "900 runs";
+    document.getElementById("perform").innerHTML ="44 Sixes"
+    document.getElementById("details").style.border="3px solid #00408c";
+    document.getElementById("details").style.background=" #cdd6e0";
+    document.getElementById("details").style.boxShadow="5px 5px 20px 10px rgba(0, 0,0, 0.5)";
+})
+
+document.getElementById("card3").addEventListener("mouseover",function(){
+    document.getElementById("name").innerHTML = "Lasith Malinga"
+    document.getElementById("age").innerHTML = "39"
+    document.getElementById("type").innerHTML = "Bowler "
+    document.getElementById("stat").innerHTML = "500 wickets";
+    document.getElementById("perform").innerHTML ="5 - 23"
+    document.getElementById("details").style.border="3px solid #00408c";
+    document.getElementById("details").style.background=" #cdd6e0";
+    document.getElementById("details").style.boxShadow="5px 5px 20px 10px rgba(0, 0,0, 0.5)";
+})
+
+document.getElementById("card4").addEventListener("mouseover",function(){
+    document.getElementById("name").innerHTML = "Shahid Afridi"
+    document.getElementById("age").innerHTML = "54"
+    document.getElementById("type").innerHTML = "All rounder "
+    document.getElementById("stat").innerHTML = "600 runs 50 wickets";
+    document.getElementById("perform").innerHTML ="100 runs 10 wickets"
+    document.getElementById("details").style.border="3px solid #00408c";
+    document.getElementById("details").style.background=" #cdd6e0";
+    document.getElementById("details").style.boxShadow="5px 5px 20px 10px rgba(0, 0,0, 0.5)";
+})
+
+document.getElementById("card4").addEventListener("mouseout",function(){
+    document.getElementById("name").innerHTML = ""
+    document.getElementById("age").innerHTML = ""
+    document.getElementById("type").innerHTML = ""
+    document.getElementById("stat").innerHTML = ""
+    document.getElementById("perform").innerHTML =""
+    document.getElementById("details").style.border="";
+    document.getElementById("details").style.background="";
+    document.getElementById("details").style.boxShadow="";
+})
+
+document.getElementById("card3").addEventListener("mouseout",function(){
+    document.getElementById("name").innerHTML = ""
+    document.getElementById("age").innerHTML = ""
+    document.getElementById("type").innerHTML = ""
+    document.getElementById("stat").innerHTML = ""
+    document.getElementById("perform").innerHTML =""
+    document.getElementById("details").style.border="";
+    document.getElementById("details").style.background="";
+    document.getElementById("details").style.boxShadow="";
+
+
+})
+
+document.getElementById("card2").addEventListener("mouseout",function(){
+    document.getElementById("name").innerHTML = ""
+    document.getElementById("age").innerHTML = ""
+    document.getElementById("type").innerHTML = ""
+    document.getElementById("stat").innerHTML = ""
+    document.getElementById("perform").innerHTML =""
+    document.getElementById("details").style.border="";
+    document.getElementById("details").style.background="";
+    document.getElementById("details").style.boxShadow="";
+
+
+})
+
+document.getElementById("card1").addEventListener("mouseout",function(){
+    document.getElementById("name").innerHTML = ""
+    document.getElementById("age").innerHTML = ""
+    document.getElementById("type").innerHTML = ""
+    document.getElementById("stat").innerHTML = ""
+    document.getElementById("perform").innerHTML =""
+    document.getElementById("details").style.border="";
+    document.getElementById("details").style.background="";
+    document.getElementById("details").style.boxShadow="";
+
+})
---
+++ gallery/gallery.js
@@ -0,0 +1,156 @@
+
+document.getElementById("img1").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("cricket_img").src = "logo.png"
+    document.getElementById("discrition").innerHTML = "The 2020 Lanka Premier League will be the inaugural edition of the Lanka Premier League (LPL) Twenty20 franchise cricket tournament in Sri Lanka. Five teams based on various Sri Lankan cities will play a total of 23 matches for a period of 15 days. "
+    
+
+});
+
+document.getElementById("img2").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("cricket_img").src = "i1.jfif";
+    document.getElementById("discrition").innerHTML = "Separamadu Lasith Malinga (Sinhala: සෙපරමාදු ලසිත් මාලිංග; born 28 August 1983) is a Sri Lankan cricketer and current T20 international captain of Sri Lanka. He captained the Sri Lankan 2014 T20 World Cup winning side. He is a right-arm fast bowler that is commonly used as a specialist death bowler, and is well known for his distinctive round-arm action, sometimes referred to as a sling action, which leads to his nicknames, 'Slinga Malinga'.. "
+    
+
+});
+
+document.getElementById("img3").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("cricket_img").src = "i3.jfif";
+    document.getElementById("discrition").innerHTML = "Galle Gladiators are a Sri Lankan franchise professional Twenty20 cricket team that competes in the Lanka Premier League, representing the city of Galle, Sri Lanka. Nadeem Omar, who owns the Quetta Gladiators in Pakistan Super League purchased the franchise in 2020. "
+    
+
+});
+
+document.getElementById("img4").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("cricket_img").src = "i4.jfif";
+    document.getElementById("discrition").innerHTML = "Galle Gladiators are a Sri Lankan franchise professional Twenty20 cricket team that competes in the Lanka Premier League, representing the city of Galle, Sri Lanka. Nadeem Omar, who owns the Quetta Gladiators in Pakistan Super League purchased the franchise in 2020. "
+    
+
+});
+
+document.getElementById("img5").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("cricket_img").src = "_AI_6932.jpg";
+    document.getElementById("discrition").innerHTML = "Galle Gladiators are a Sri Lankan franchise professional Twenty20 cricket team that competes in the Lanka Premier League, representing the city of Galle, Sri Lanka. Nadeem Omar, who owns the Quetta Gladiators in Pakistan Super League purchased the franchise in 2020. "
+    
+
+});
+
+document.getElementById("img6").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("cricket_img").src = "b2.jfif";
+    document.getElementById("discrition").innerHTML = "Galle Gladiators are a Sri Lankan franchise professional Twenty20 cricket team that competes in the Lanka Premier League, representing the city of Galle, Sri Lanka. Nadeem Omar, who owns the Quetta Gladiators in Pakistan Super League purchased the franchise in 2020. "
+    
+
+});
+
+document.getElementById("img7").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("cricket_img").src = "b3.jfif";
+    document.getElementById("discrition").innerHTML = "Galle Gladiators are a Sri Lankan franchise professional Twenty20 cricket team that competes in the Lanka Premier League, representing the city of Galle, Sri Lanka. Nadeem Omar, who owns the Quetta Gladiators in Pakistan Super League purchased the franchise in 2020. "
+    
+
+});
+
+document.getElementById("img8").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("cricket_img").src = "download.jpg";
+    document.getElementById("discrition").innerHTML = "Galle Gladiators are a Sri Lankan franchise professional Twenty20 cricket team that competes in the Lanka Premier League, representing the city of Galle, Sri Lanka. Nadeem Omar, who owns the Quetta Gladiators in Pakistan Super League purchased the franchise in 2020. "
+    
+
+});
+
+document.getElementById("img9").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("cricket_img").src = "DSC_8766.jpg";
+    document.getElementById("discrition").innerHTML = "Galle Gladiators are a Sri Lankan franchise professional Twenty20 cricket team that competes in the Lanka Premier League, representing the city of Galle, Sri Lanka. Nadeem Omar, who owns the Quetta Gladiators in Pakistan Super League purchased the franchise in 2020. "
+    
+
+});
+
+document.getElementById("img10").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("cricket_img").src = "images.jpg";
+    document.getElementById("discrition").innerHTML = "Galle Gladiators are a Sri Lankan franchise professional Twenty20 cricket team that competes in the Lanka Premier League, representing the city of Galle, Sri Lanka. Nadeem Omar, who owns the Quetta Gladiators in Pakistan Super League purchased the franchise in 2020. "
+    
+
+});
+
+document.getElementById("img11").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("cricket_img").src = "lpl2.jpg";
+    document.getElementById("discrition").innerHTML = "Galle Gladiators are a Sri Lankan franchise professional Twenty20 cricket team that competes in the Lanka Premier League, representing the city of Galle, Sri Lanka. Nadeem Omar, who owns the Quetta Gladiators in Pakistan Super League purchased the franchise in 2020. "
+    
+
+});
+
+document.getElementById("img12").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("cricket_img").src = "lpl3.jfif";
+    document.getElementById("discrition").innerHTML = "Galle Gladiators are a Sri Lankan franchise professional Twenty20 cricket team that competes in the Lanka Premier League, representing the city of Galle, Sri Lanka. Nadeem Omar, who owns the Quetta Gladiators in Pakistan Super League purchased the franchise in 2020. "
+    
+
+});
+
+document.getElementById("img13").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("cricket_img").src = "SAM_4211.jpg";
+    document.getElementById("discrition").innerHTML = "Galle Gladiators are a Sri Lankan franchise professional Twenty20 cricket team that competes in the Lanka Premier League, representing the city of Galle, Sri Lanka. Nadeem Omar, who owns the Quetta Gladiators in Pakistan Super League purchased the franchise in 2020. "
+    
+
+});
+
+document.getElementById("img14").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("cricket_img").src = "slpl.jpg";
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("discrition").innerHTML = "Galle Gladiators are a Sri Lankan franchise professional Twenty20 cricket team that competes in the Lanka Premier League, representing the city of Galle, Sri Lanka. Nadeem Omar, who owns the Quetta Gladiators in Pakistan Super League purchased the franchise in 2020. "
+    
+
+});
+
+document.getElementById("img15").addEventListener("click",function(){
+
+    document.getElementById("player-view").style.visibility="visible"
+    document.getElementById("back-btn").style.visibility="visible";
+    document.getElementById("cricket_img").src = "4K1L0120.jpg";
+    document.getElementById("discrition").innerHTML = "Galle Gladiators are a Sri Lankan franchise professional Twenty20 cricket team that competes in the Lanka Premier League, representing the city of Galle, Sri Lanka. Nadeem Omar, who owns the Quetta Gladiators in Pakistan Super League purchased the franchise in 2020. "
+    
+
+});
+
+
+document.getElementById("back-btn").addEventListener("click",function(){
+    document.getElementById("player-view").style.visibility="hidden";
+    document.getElementById("back-btn").style.visibility="hidden";
+});
